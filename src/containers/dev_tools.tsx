43a6ef5adc0b0b8a5f1564da2264d880622e1653
remojansen <remo.jansen@wolksoftware.com>
2016-10-05 20:48:01 +0100

    spelling from code review

--- src/containers/dev_tools.tsx
+++ src/containers/dev_tools.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
-import * as ReduxDevtols from "redux-devtools";
+import * as ReduxDevtools from "redux-devtools";
 import LogMonitor from "redux-devtools-log-monitor";
 import DockMonitor from "redux-devtools-dock-monitor";
 
-let DevTools: any = ReduxDevtols.createDevTools(
+let DevTools: any = ReduxDevtools.createDevTools(
     <DockMonitor toggleVisibilityKey="ctrl-h" changePositionKey="ctrl-w">
         <LogMonitor />
     </DockMonitor>
